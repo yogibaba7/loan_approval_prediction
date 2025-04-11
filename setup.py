3f5fe3756c1c7e40fd7ff49f1b23b42e59169c90
yogibaba7 <yogeshchouhan263@gmail.com>
2025-04-12 00:26:49 +0530

    project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is related to Financial and Banking domain. In this project we will predict whether the loan application should be accepted or rejected based on the information regarding income of the loan applicant, loan amount, number of dependents, credit history etc.',
+    author='Yogesh chouhan',
+    license='',
+)
