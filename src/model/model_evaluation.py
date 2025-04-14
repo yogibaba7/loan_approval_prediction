77c1bb991c40a981d98fe53bf6fd289223abc417
yogibaba7 <yogeshchouhan263@gmail.com>
2025-04-14 11:49:05 +0530

    add dvc pipeline exp

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -6,7 +6,6 @@ import logging
 import joblib
 import yaml
 import json
-
 from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score
 
 # configure logging
@@ -30,7 +29,7 @@ def load_data(path:str)->tuple[pd.DataFrame]:
 # load params
 
 def load_params()->dict:
-    logger.debug(f"loading {parameter}")
+    logger.debug(f"loading parameters")
     try:
         with open('params.yaml') as f:
             file = yaml.safe_load(f)
@@ -72,20 +71,63 @@ def evaluate_results(model,X_test,y_test):
     except Exception as e:
         logger.error(f"error occured -> {e}")
 
+# save model_info
+def save_model_info(run_id,model_path,file_path):
+    logger.debug(f"saving the model info in {file_path}")
+    try:
+        model_info = {'run_id':run_id,'model_path':model_path}
+        with open(file_path,'w') as f:
+            json.dump(model_info,f,indent=4)
+    except Exception as e:
+        logger.error(f"error occured -->{e}")
+
+#CONFIGURE EXPERIMENT
+import mlflow
+import dagshub
+from mlflow.models.signature import infer_signature
+dagshub.init(repo_owner='yogibaba7', repo_name='loan_approval_prediction', mlflow=True)
+# set tracking uri
+mlflow.set_tracking_uri('https://dagshub.com/yogibaba7/loan_approval_prediction.mlflow/')
+# create a experiment
+mlflow.set_experiment('pipeline_exp')
+
 # main
 def main():
-    data_path = 'data/processed/test_processed.csv'
-    # load testing data  for model evaluation
-    test_data = load_data(data_path)
-    # make x_test and y_test
-    X_test = test_data.drop(columns=['Loan_Status'])
-    y_test = test_data['Loan_Status']
+    with mlflow.start_run(description='This is the run of best model with best parameters') as run:
 
-    # load model
-    model = load_model()
+        data_path = 'data/processed/test_processed.csv'
+        # load testing data  for model evaluation
+        test_data = load_data(data_path)
+        # make x_test and y_test
+        X_test = test_data.drop(columns=['Loan_Status'])
+        y_test = test_data['Loan_Status']
+        # load model
+        model = load_model()
+        # evaluate results
+        results = evaluate_results(model,X_test,y_test)
+        # load parameters of model_building
+        parameters = load_params()
 
-    # evaluate results
-    results = evaluate_results(model,X_test,y_test)
+        # create model signature
+        model_signature = infer_signature(X_test,y_test)
+        # log model
+        mlflow.sklearn.log_model(model,'AdaBoostClassifier',signature=model_signature)
+        # log model parameters
+        mlflow.log_params(parameters)
+        # log model result metrics
+        mlflow.log_metrics(results)
+        # log metrics file to mlflow
+        mlflow.log_artifact('reports/metrics.json')
+        # set some tags 
+        mlflow.set_tag('author','Yogesh chouhan')
+        # save the model info
+        run_id = run.info.run_id
+        model_path = 'AdaBoostClassifier'
+        save_model_info(run_id=run_id,model_path=model_path,file_path='reports/model_info.json')
+        # log the model_info to mlflow
+        mlflow.log_artifact('reports/model_info.json')
+
+        
 
 if __name__=="__main__":
     main()
